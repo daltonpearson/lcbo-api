5e200c3470661491dbf2028cdcc8e02ac924d3c0
Dalton Pearson <daltonpearson1997@gmail.com>
2019-02-04 21:10:20 -0500

    fixing files structure

--- constants.py
+++
@@ -1,4 +0,0 @@
-from constants import *
-
-API_BASE_URL = 'http://www.foodanddrink.ca/lcbo-webapp/'
-NUM_PRODUCTS = 5#15000
--- src/crawler.py
+++ src/crawler.py
@@ -1,7 +1,5 @@
 import xml.etree.ElementTree as ET
 import requests
-import products
-from constants import *
 
 API_BASE_URL = 'http://www.foodanddrink.ca/lcbo-webapp/'
 NUM_PRODUCTS = 5#15000
